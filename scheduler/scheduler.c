a3353d6cae8a7acc2b0d5f8fe92c556225368c32
walid <walid@walid-Precision-WorkStation-380.(none)>
2014-05-16 19:32:28 +0200

    bugs fixed and compiled

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#include <native/alarm.h>
 #include <native/task.h>
 #include <native/timer.h>
 #include <rtdk.h>
@@ -11,26 +12,26 @@
 #define TIMER_READ rt_timer_read()-REF
 #define TASK_NUMBER 4
 #define SECOND 1000000000
-static RT_ALARM alarm;
+static RT_ALARM alarme;
 
 int hp=2;
-
+int REF = 0;
 typedef  struct Element_tab_ordo {
  char name[16];
  int id;
  int duree;
  int create_thread; 
- void *(*code)();
+ void (*code)(void*);
 }ELEMENT_TAB ;
 
 
 RT_TASK task[TASK_NUMBER];
-const SIZE_T=6;
+const int size_table=6;
 int i=-1;
 int i_prec=0;
 int j=0;
 
-ELEMENT_TAB T[]={{"task2",2,2,1,task2},{"task1",1,2,1,task1},{"task2",2,2,0,task2},{"task3",3,2,1,task3},{"task1",1,2,1,task1},{"task_vide",4,2,1,task_vide}};
+
 
 void task1(void* unsued)          /// (2,2,6,6)
 {
@@ -64,16 +65,16 @@ void task_vide(void* unsued)	/// free time filler task
   rt_printf("[%lld]Fin task_vide\n[%lld]Hyperperiod : %d\n", TIMER_READ,TIMER_READ,hp);
 }
 /// *********************SCHEDULER******************** ///
-
-void scheduler()
+ELEMENT_TAB T[]={{"task2",2,2,1,task2},{"task1",1,2,1,task1},{"task2",2,2,0,task2},{"task3",3,2,1,task3},{"task1",1,2,1,task1},{"task_vide",4,2,1,task_vide}};
+void scheduler(void* unused)
 {
   while(j++<50)
   {
-  rt_alarm_wait(&alarm);
+  rt_alarm_wait(&alarme);
   
-  if(i==(SIZE_T-1)){
+  if(i==(size_table-1)){
     i_prec=i;
-    i=I_PERM;
+    i=0;
   }
   else {
     i_prec=i;
@@ -81,7 +82,7 @@ void scheduler()
   } 
   /// if the previous thread is preempted (i.e it is running )and we are not in the initial phase ( i != -1 )
   /// we suspend it
-  if((i_prec!=(-1))&&(i!=(-1))&&(task[(T[i_prec].id-1)]) ){
+  if( (i_prec!=(-1)) && (i!=(-1)) && i==1 ){ 									//WARNING (task[(T[i_prec].id-1)]) ){
        rt_task_suspend(&(task[(T[i_prec].id-1)]));
   }
   
@@ -99,25 +100,25 @@ void scheduler()
    rt_printf("[%lld]Reprise task %d\n", TIMER_READ, T[i].id);
    rt_task_resume(&(task[(T[i_prec].id-1)]));    
   }
-  rt_alarm_start(& alarm, T[i].duree * SECOND, TM_INFINITE);
+  rt_alarm_start(& alarme, T[i].duree * SECOND, TM_INFINITE);
   }
 }    
   
 
 int main()
 {
-  int REF = 0;
+  int err;
   RT_TASK ORD;
   mlockall(MCL_CURRENT|MCL_FUTURE);
   rt_print_auto_init(1);
   ///***********************************************************************/
-  if((err=rt_alarm_create(& alarm, "timetrigger_ordonnanceur")) !=0) {
+  if((err=rt_alarm_create(& alarme, "timetrigger_ordonnanceur")) !=0) {
     exit(EXIT_FAILURE);}
   
   ///***********************************************************************/ 
   
   
-  rt_task_spawn(ORD,
+  rt_task_spawn(&ORD,
 		"scheduler",
 		0,
 		99,
@@ -127,15 +128,15 @@ int main()
   
   rt_task_sleep(1000000000LL); 	///sleep one second
   REF = rt_timer_read(); 	/// set time reference
-  if((err=rt_alarm_start(& alarm, TM_NOW, TM_INFINITE))!= 0){
+  if((err=rt_alarm_start(& alarme, TM_NOW, TM_INFINITE))!= 0){
     exit(EXIT_FAILURE);}
   
-  rt_task_join(ORD);
-  rt_task_join(task[0]);
-  rt_task_join(task[1]);
-  rt_task_join(task[2]);
-  rt_task_join(task[3]);
-  rt_alarm_delete(& alarm);
+  rt_task_join(&ORD);
+  rt_task_join(&(task[0]));
+  rt_task_join(&(task[1]));
+  rt_task_join(&(task[2]));
+  rt_task_join(&(task[3]));
+  rt_alarm_delete(& alarme);
   
  return 0; 
 }
